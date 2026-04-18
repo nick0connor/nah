2e7c1b2bb07e881199ef57d0bb695c920476ee49
Nick O'Connor <nioc4468@colorado.edu>
2026-04-18 16:39:25 -0500

    search bar + temp code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+  // Media Type
   const [mediaType, setMediaType] = useState("movie")
   const radios = [
     { name:"Movie",   value:"movie" },
     { name:"TV Show", value:"tvshow" }
   ]
+
+  // Searchbox
+  const [isLoading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  var query = "";
+
+  const handleSearchClick = (e) => {
+    e.preventDefault();
+
+    query = searchText;
+    console.log(`Query: '${query}'`);
+    
+    setLoading(true)
+  };
+
+  // Temp code for searchbox - replace with real network request to backend
+  useEffect(() => { 
+    function simulateNetworkRequest() {
+      return new Promise(resolve => {
+        setTimeout(resolve, 2000);
+      });
+    }
+
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+  
 
   return (
     <>
@@ -31,7 +64,36 @@ function App() {
         </ButtonGroup>
       </section>
 
-      
+      <section className='searchBox'>
+        <Form 
+          className='d-flex align-items-center gap-2 w-100'
+          onSubmit={handleSearchClick}
+        >
+          <Form.Control 
+            type="text" 
+            size='lg'
+            className='flex-grow-1'
+            style={{ flex: "0 0 80%" }}
+            placeholder={mediaType == "movie" ? "Movie name" : "Show name"}
+            value={searchText}
+            disabled={isLoading}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+
+          <Button 
+            variant='primary'
+            size='lg'
+            disabled={isLoading}
+            style={{ flex: "0 0 20%" }}
+            type='submit'
+          >
+            {isLoading ? 'Searching...' : 'Search'}
+          </Button>
+        </Form>
+
+        <p>{searchText}</p>
+      </section>
+
     </>
   )
 }
