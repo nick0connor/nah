f150e02a040b519c3cf0c58e5d3cffbc77378d34
Nick O'Connor <nioc4468@colorado.edu>
2026-04-19 11:38:35 -0500

    added webtorrent functionality

--- server/index.mjs
+++ server/index.mjs
@@ -1,6 +1,9 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import TorrentSearchApi from 'torrent-search-api';
+import WebTorrent from "webtorrent";
+
+import config from '../config.paths.json' with { type: 'json' };
 
 const app = express();
 app.use(express.json());
@@ -8,12 +11,15 @@ app.use(cors({
   origin: "http://localhost:5173"
 }));
 
+const client = new WebTorrent();
+
 TorrentSearchApi.enablePublicProviders();
 var mostRecentTorrent = [];
+var mediaType = null;
 
 app.post("/search", async (req, res) => {
   const query = req.body.query;
-  const mediaType = (req.body.media == "movie") ? "Movies" : "TV";
+  mediaType = (req.body.media == "movie") ? "Movies" : "TV";
 
   console.log(`Received ${mediaType}: '${query}'`)
 
@@ -21,6 +27,30 @@ app.post("/search", async (req, res) => {
 
   res.json(mostRecentTorrent);
 });
+
+
+function addTorrentAsync(client, magnet) {
+  return new Promise((resolve, reject) => {
+    client.add(magnet, { path: config[mediaType] }, (torrent) => {
+
+      torrent.on('download', () => {
+        const progress = (torrent.progress * 100).toFixed(2);
+        const speed = (torrent.downloadSpeed / 1048576).toFixed(2);
+
+        process.stdout.write(`\rProgress: ${progress}% | Speed: ${speed} MB/s`)
+      });
+
+      torrent.on("done", () => {
+        process.stdout.write("\n");
+        console.log("Download complete:", torrent.infoHash);
+      });
+
+      torrent.on("error", reject);
+
+      resolve(torrent);
+    });
+  })
+}
 
 app.post("/confirm", async (req, res) => {
   if(!mostRecentTorrent || mostRecentTorrent.length === 0) {
@@ -30,7 +60,18 @@ app.post("/confirm", async (req, res) => {
   const selected = mostRecentTorrent[parseInt(req.body.index)];
   const magnet = await TorrentSearchApi.getMagnet(selected);
 
-  res.json({response: "success", magnet: magnet});
+  try {
+    const torrent = await addTorrentAsync(client, magnet);
+    console.log(`Downloading ${torrent.infoHash}`);
+
+    res.json({
+      response: "started",
+      infoHash: torrent.infoHash
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to start torrent" });
+  }
 });
 
 app.listen(3000, "0.0.0.0", () => {
