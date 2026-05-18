0a100e34dc98d82f8936f4a9ffe6ccd4c3311e40
Nick O'Connor <nioc4468@colorado.edu>
2026-05-18 17:44:40 -0500

    Reworked to use state.mjs

--- server/routes/search.mjs
+++ server/routes/search.mjs
@@ -1,19 +1,18 @@
 import express from "express";
 import TorrentSearchApi from 'torrent-search-api';
+import { mostRecentTorrent, mediaType, setMostRecentTorrent, setMediaType } from "../state.mjs";
 
 const router = express.Router();
 
 TorrentSearchApi.enablePublicProviders();
-var mostRecentTorrent = [];
-var mediaType = null;
 
 router.post("/search", async (req, res) => {
   const query = req.body.query;
-  mediaType = req.body.media;
+  setMediaType(req.body.media);
 
   console.log(`Received ${mediaType}: '${query}'`)
 
-  mostRecentTorrent = await TorrentSearchApi.search(query, mediaType, 20);
+  setMostRecentTorrent(await TorrentSearchApi.search(query, mediaType, 20));
 
   res.json(mostRecentTorrent);
 });
