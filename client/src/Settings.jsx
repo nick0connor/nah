9a86385332267e345c198ec47782a0f898ec71ba
Nick O'Connor <nioc4468@colorado.edu>
2026-05-18 12:24:07 -0500

    created settings page

---
+++ client/src/Settings.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function Settings() {
+
+  return (
+    <h1 style={{color:"white"}}>Settings</h1>
+  );
+}
+
+export default Settings;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './style/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
+import Settings from './Settings.jsx'
+
+const page = window.location.pathname;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='contentPad'>
-      <App />
+      {page === '/settings' ? <Settings /> : <App />}
     </div>
   </StrictMode>,
 )
